00764c6ffd29f3a9e67dd4db0a5cf268c63d5fdd
Talon <ak1998@live.nl>
2026-03-06 01:12:41 +0100

    created chat

--- lib/crow/chat/chat.ex
+++ lib/crow/chat/chat.ex
@@ -2,7 +2,7 @@ defmodule Crow.Chat do
   def list_his_messages do
     [
       %{content: "Hi sweetie", time_stamp: "12:10"},
-      %{content: "That's not nice.", time_stamp: "12:15"}
+      %{content: "Did you leave me on read?", time_stamp: "12:15"}
     ]
   end
 end
