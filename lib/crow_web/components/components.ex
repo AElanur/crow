be1396db56421c118129fe6821b13aa43cec09bb
Talon <ak1998@live.nl>
2026-03-17 18:04:51 +0100

    layout updated

--- lib/crow_web/components/components.ex
+++ lib/crow_web/components/components.ex
@@ -4,12 +4,24 @@ defmodule Components do
   attr :field, :any, required: true
   attr :label, :string, default: nil
   attr :type, :string, default: "text"
+  attr :placeholder, :string, default: "placeholder"
   attr :rest, :global
 
   def input(assigns) do
     ~H"""
       <label><%= @label %></label>
-      <input class="border h-[40px] px-3 rounded-sm" type="text"/>
+      <input
+        class="bg-[#272728] shadow-xl h-[40px] px-3 rounded-sm"
+        type={@type}
+        placeholder={@placeholder}/>
+    """
+  end
+
+  attr :rest, :global, include: ~w(form)
+  attr :name, :string, default: "submit"
+  def button(assigns) do
+    ~H"""
+      <input {@rest} type="button" value={@name} class="bg-[#272728] w-full h-[3rem]  rounded-sm"/>
     """
   end
 end
