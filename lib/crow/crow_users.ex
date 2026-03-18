c6ad71d3c7a3bded2a5e11fcbd3644a27efc3139
Talon <ak1998@live.nl>
2026-03-18 14:44:11 +0100

    Base insert implemtented, test script + form control missing

---
+++ lib/crow/crow_users.ex
@@ -0,0 +1,106 @@
+defmodule Crow.CrowUsers do
+  @moduledoc """
+  The Users context.
+  """
+
+  import Ecto.Query, warn: false
+  import Logger
+  alias Crow.Repo
+
+  alias Crow.CrowUsers.CrowUser
+
+  @doc """
+  Returns the list of users.
+
+  ## Examples
+
+      iex> list_users()
+      [%User{}, ...]
+
+  """
+  def list_users do
+    Repo.all(CrowUser)
+  end
+
+  @doc """
+  Gets a single user.
+
+  Raises `Ecto.NoResultsError` if the User does not exist.
+
+  ## Examples
+
+      iex> get_user!(123)
+      %User{}
+
+      iex> get_user!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_user!(id), do: Repo.get!(CrowUser, id)
+
+  @doc """
+  Creates a user.
+
+  ## Examples
+
+      iex> create_user(%{field: value})
+      {:ok, %User{}}
+
+      iex> create_user(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_user(attrs) do
+    Logger.info(inspect(attrs))
+    %CrowUser{}
+    |> CrowUser.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a user.
+
+  ## Examples
+
+      iex> update_user(user, %{field: new_value})
+      {:ok, %User{}}
+
+      iex> update_user(user, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_user(%CrowUser{} = user, attrs) do
+    user
+    |> CrowUser.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a user.
+
+  ## Examples
+
+      iex> delete_user(user)
+      {:ok, %User{}}
+
+      iex> delete_user(user)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_user(%CrowUser{} = user) do
+    Repo.delete(user)
+  end
+
+  # @doc """
+  # Returns an `%Ecto.Changeset{}` for tracking user changes.
+
+  # ## Examples
+
+  #     iex> change_user(user)
+  #     %Ecto.Changeset{data: %User{}}
+
+  # """
+  # # def change_user(%CrowUser{} = user, attrs \\ %{}) do
+  # #   User.changeset(user, attrs)
+  # # end
+end
--- lib/crow_web/live/user/register_view.ex
+++ lib/crow_web/live/user/register_view.ex
@@ -2,6 +2,8 @@ defmodule CrowWeb.RegisterView do
   require Logger
   use Phoenix.LiveView
 
+  alias Crow.CrowUsers
+
    def mount(_params, _session, socket) do
     form =
       to_form(%{
@@ -29,8 +31,8 @@ defmodule CrowWeb.RegisterView do
               <.form class="flex flex-col space-y-2" as={:user} for={@form} phx-submit="submit">
                 <Components.input field={@form[:email]} type={"text"} label={"Email"} placeholder={"Email"}/>
                 <Components.input field={@form[:username]} type={"text"} label={"Username"} placeholder={"Username"}/>
-                <Components.input field={@form[:password]} type={"password"} label={"Password"} placeholder={"Password"}/>
-                <Components.input field={@form[:password_confirmation]} type={"password"} label={"Confirm password"} placeholder={"Confirm password"}/>
+                <Components.input field={@form[:first_password]} type={"password"} label={"Password"} placeholder={"Password"}/>
+                <Components.input field={@form[:password]} type={"password"} label={"Confirm password"} placeholder={"Confirm password"}/>
               <div class="flex justify-end pt-5">
                 <Components.button name={"Register"}/>
               </div>
@@ -43,8 +45,20 @@ defmodule CrowWeb.RegisterView do
   end
 
   def handle_event("submit", params, socket) do
-    Logger.info(inspect(params))
+    action = socket.assigns.live_action || :new
+    create_user(socket, action, params)
+  end
 
-    {:noreply, socket}
+  defp create_user(socket, :new, user_params) do
+    case CrowUsers.create_user(user_params) do
+      {:ok, _user} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "User created successfully")
+         |> push_navigate(to: "/")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
   end
 end
